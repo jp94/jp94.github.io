de8cd42e29c7a11df873bc3826b7bca0c10c4188
James Park <94jamespark@gmail.com>
2017-06-13 22:45:22 -0400

    Old webpage import

---
+++ js/svgAnimator.js
@@ -0,0 +1,30 @@
+// Pseudo-random background color (of my choice ofc :P)
+var MIN_RANGE = 30, MAX_RANGE = 100;
+var colors = [MIN_RANGE, MAX_RANGE, getRandomIntRange(MIN_RANGE, MAX_RANGE)];
+var temp;
+
+// Shuffle RGB values
+for (var i = 0; i < colors.length; i++) {
+    var index = getRandomIntRange(0, colors.length - 1);
+    temp = colors[index];
+    colors[index] = colors[i];
+    colors[i] = temp;
+}
+
+document.body.style.background = "rgb(" + colors[0] + ", " + colors[1] + "," + colors[2] + ")";
+
+
+
+// SVG animation settings
+var transitionType = ["async", "oneByOne", "delayed"];
+
+new Vivus('svg', {
+    type: transitionType[getRandomIntRange(0, transitionType.length - 1)],
+    duration: 150
+});
+
+// Helper method for getting a random integer in a range (inclusive)
+function getRandomIntRange(min, max) {
+    return parseInt((Math.random() * (max - min + 1)) + min);
+}
+
